530edab84ba3150c2f1b6d18f814ea5d4b7004b1
rohan-bhxtia <rohanbhatia003@gmail.com>
2025-10-17 17:06:34 +0530

    ..

---
+++ LL/2printLL.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+    #########################################################
+    def print(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
+
+
+    
+        
+
+
+my_LL = LinkedList(3)
+print("head:" ,my_LL.head.value)  
+my_LL.print()
