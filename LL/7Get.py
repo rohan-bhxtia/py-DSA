8770608d17600da4bf31e64b09ce98366cc9b7f9
rohan-bhxtia <rohanbhatia003@gmail.com>
2025-11-08 20:24:00 +0530

    ..

---
+++ LL/7Get.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class linkedlist:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length =1
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length +=1      
+
+    def Get(self,index):
+        if index <0 or index >= self.length :
+            return None
+        temp = self.head
+        for i in range (index):
+            temp = temp.next
+        return temp    
+    
+
+ll = linkedlist(22)
+ll.append(3)
+ll.append(44)
+
+get= ll.Get(0)
+print(get.value)
+
+
+
+
+
+
