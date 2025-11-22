2dfd63cc07817e0f49152e819590079a41109196
Rohan ! <149778795+rohan-bhxtia@users.noreply.github.com>
2025-11-22 23:43:34 +0530

    Update 7Get.py

--- LL/7Get.py
+++ LL/7Get.py
@@ -9,7 +9,7 @@ class linkedlist:
         self.head = new_node
         self.tail = new_node
         self.length =1
-##
+
     def append(self,value):
         new_node = Node(value)
         if self.head is None:
